10995f7754afc0a1c052ae7d311259a3c73a8711
Mpiloenhle thandolwethu Ntanda <125492323+Codedeveloper-MT@users.noreply.github.com>
2023-07-25 05:06:34 +0200

    Add files via upload

---
+++ Learning_a.java
@@ -0,0 +1,15 @@
+
+//sending output
+public class Learning_a{
+	public static void main(String[]args){
+		//activity 2 of ppa
+		System.out.println("Hellow Thando");
+		System.out.println("Holla bafethu");
+		System.out.println("6+4="+6+4);
+		System.out.println("6+4="+(6+4));
+		
+		
+		
+	}
+}
+	
